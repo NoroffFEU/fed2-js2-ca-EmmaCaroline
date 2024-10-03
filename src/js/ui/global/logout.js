0ff3fe1cf9e2bbce64b35cd8fc77338f914c2e25
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-03 20:39:24 +0200

    Created id for logout button, and created logout function and event listener for it

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,1 +1,10 @@
-export function setLogoutListener() {}
+import { onLogout } from "../auth/logout";
+
+export function setLogoutListener() {
+  const logoutBtn = document.getElementById("logout-btn");
+  if (logoutBtn) {
+    logoutBtn.addEventListener("click", onLogout);
+  }
+}
+
+// REMEMBER: I need to find the right place to call this function
