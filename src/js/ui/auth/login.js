a5e6d90e32b250616908e497fc3edab95255b7be
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-03 19:55:09 +0200

    Created function for form data, ui auth login

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,18 @@
-export async function onLogin(event) {}
+import { login } from "../../api/auth/login";
+
+export async function onLogin(event) {
+  event.preventDefault();
+
+  const form = document.forms.login;
+  const formData = new FormData(form);
+  const user = Object.fromEntries(formData.entries());
+
+  try {
+    const response = await login(user);
+    console.log("Login ok", response);
+    alert(`Login successful. Hello ${user.name}`);
+    window.location.href = "/auth/login/";
+  } catch (error) {
+    console.error("Login failed:", error);
+  }
+}
