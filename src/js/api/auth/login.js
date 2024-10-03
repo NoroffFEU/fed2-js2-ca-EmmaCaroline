61e3cdb8e594216b57a51ed66aa154656ba9b3ef
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-03 02:36:23 +0200

    Created function for API auth login with error handling

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,38 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN } from "../constants";
+import { save } from "./key";
+import { headers } from "../headers";
+
+export async function login({ email, password }) {
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const response = await fetch(API_AUTH_LOGIN, {
+      headers: headers(),
+      method: "POST",
+      body,
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Login failed: ${errorText}`);
+    }
+
+    const result = await response.json();
+    save("token", result.data.accessToken);
+    save("user", result.data);
+
+    alert("You are now logged in"); //Remove later when working as it should
+
+    window.location.href = "/";
+
+    return result; //Do I need this? check later
+  } catch (error) {
+    if (error.name === "TypeError") {
+      alert("Network error, try again later");
+    } else {
+      alert(`Login failed: ${error.message}`);
+    }
+    console.error("Login failed", error);
+    throw error;
+  }
+}
--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -2,11 +2,7 @@ import { API_AUTH_REGISTER } from "../constants";
 import { headers } from "../headers";
 
 export async function register({ name, email, password }) {
-  const body = JSON.stringify({
-    name,
-    email,
-    password,
-  });
+  const body = JSON.stringify({ name, email, password });
 
   try {
     const response = await fetch(API_AUTH_REGISTER, {
