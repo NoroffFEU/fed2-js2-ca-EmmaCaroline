162f49b8259c261b35c9012ed127c3dff040a0b1
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-05 04:32:32 +0200

    Created API call function for create post

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,28 @@
-export async function createPost({ title, body, tags, media }) {}
+import { API_SOCIAL_POSTS } from "../constants";
+import { headers } from "../headers";
+
+export async function createPost({ title, body, tags, media }) {
+  try {
+    const response = await fetch(API_SOCIAL_POSTS, {
+      headers: headers(),
+      method: "POST",
+      body: JSON.stringify({ title, body, tags, media }),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error("Failed to create post: " + errorText);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    if (error.name === "TypeError") {
+      alert("Network error, try again later");
+    } else {
+      alert(`Creating post failed: ${error.message}`);
+    }
+    console.error("Creating post failed", error);
+    throw error;
+  }
+}
