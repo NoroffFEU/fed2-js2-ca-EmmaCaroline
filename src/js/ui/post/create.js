fcb6735a2037f0d1a59efe1f703f7d30a7cc55b0
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-05 17:53:55 +0200

    Created function for creating a post

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,22 @@
-export async function onCreatePost(event) {}
+import { createPost } from "../../api/post/create";
+
+export async function onCreatePost(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const post = Object.fromEntries(formData.entries());
+
+  // Set tags to an empty array if not provided
+  post.tags = post.tags ? post.tags.split(",").map((tag) => tag.trim()) : []; // Split by comma and trim spaces
+
+  if (post.media) {
+    post.media = {
+      url: post["media-url"],
+      alt: post["media-alt"] || "No description provided",
+    };
+  }
+
+  const response = await createPost(post);
+  alert("Post created!");
+  form.reset();
+}
