65c4e0720abf29409b5139b183d5dc5eb809fbaf
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-06 18:51:25 +0200

    Added back mistakenly removed function

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -2,34 +2,6 @@ import { API_SOCIAL_POSTS } from "../constants";
 import { API_SOCIAL_PROFILES } from "../constants";
 import { headers } from "../headers";
 
-export async function readPost(id, includeAuthor = true) {
-  const queryParams = new URLSearchParams({
-    _author: includeAuthor ? "true" : "false",
-  });
-  if (isNaN(id)) {
-    throw new Error("Invalid post ID: must be a number");
-  }
-
-  try {
-    const endpoint = `${API_SOCIAL_POSTS}/${id}?${queryParams.toString()}`;
-    const response = await fetch(endpoint, {
-      headers: headers(),
-      method: "GET",
-    });
-
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error("Failed to fetch post: " + errorText);
-    }
-
-    const postData = await response.json();
-    return postData.data;
-  } catch (error) {
-    console.error("Fetching post failed: ", error);
-    throw error;
-  }
-}
-
 export async function readPosts(limit = 12, page = 1, tag) {
   const endpoint = new URL(API_SOCIAL_POSTS);
   endpoint.searchParams.append("_author", "true");
@@ -55,6 +27,34 @@ export async function readPosts(limit = 12, page = 1, tag) {
     return postsData;
   } catch (error) {
     console.error("Fetching posts failed: ", error);
+    throw error;
+  }
+}
+
+export async function readPost(id, includeAuthor = true) {
+  const queryParams = new URLSearchParams({
+    _author: includeAuthor ? "true" : "false",
+  });
+  if (isNaN(id)) {
+    throw new Error("Invalid post ID: must be a number");
+  }
+
+  try {
+    const endpoint = `${API_SOCIAL_POSTS}/${id}?${queryParams.toString()}`;
+    const response = await fetch(endpoint, {
+      headers: headers(),
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error("Failed to fetch post: " + errorText);
+    }
+
+    const postData = await response.json();
+    return postData.data;
+  } catch (error) {
+    console.error("Fetching post failed: ", error);
     throw error;
   }
 }
