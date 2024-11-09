43c6bb070634ce42c631abd6e0be45a4cddd2fe5
EmmaCaroline <emma-caroline@hotmail.com>
2024-11-09 04:03:32 +0100

    Made the header/navigation

--- src/app.js
+++ src/app.js
@@ -3,3 +3,11 @@ import "./css/style.css";
 import router from "./js/router";
 
 await router(window.location.pathname);
+
+// Header Navigation event listener
+const menuBtn = document.querySelector("#menu-btn");
+const menu = document.querySelector("#mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
