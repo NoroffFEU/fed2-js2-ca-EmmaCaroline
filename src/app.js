55594f7b68ecc07a95c976281395a0a91c295c62
EmmaCaroline <emma-caroline@hotmail.com>
2024-11-09 20:44:06 +0100

    Implemented dark mode theme toggle

--- src/app.js
+++ src/app.js
@@ -11,3 +11,14 @@ const menu = document.querySelector("#mobile-menu");
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+// Dark mode theme toggle function and event listener
+function toggleTheme() {
+  const isDarkMode = document.documentElement.classList.toggle("dark");
+
+  document.querySelector("#sun").classList.toggle("hidden", isDarkMode);
+  document.querySelector("#moon").classList.toggle("hidden", !isDarkMode);
+}
+
+const themeToggle = document.querySelector("#theme-toggle");
+themeToggle.addEventListener("click", toggleTheme);
