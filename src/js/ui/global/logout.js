7f73c34c712a08068ff8c840298aa4babd1d044e
EmmaCaroline <emma-caroline@hotmail.com>
2024-11-09 04:18:27 +0100

    Added an confirmation alert to logout button

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -3,6 +3,13 @@ import { onLogout } from "../auth/logout";
 export function setLogoutListener() {
   const logoutBtn = document.getElementById("logout-btn");
   if (logoutBtn) {
-    logoutBtn.addEventListener("click", onLogout);
+    logoutBtn.addEventListener("click", () => {
+      const userConfirmed = confirm("Are you sure you want to log out?");
+      if (userConfirmed) {
+        onLogout();
+      } else {
+        window.location.reload();
+      }
+    });
   }
 }
